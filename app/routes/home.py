42e541fb820a6575abd6666f69a0d3a21b1de4da
PrxBxny <konotop.ilya1304@gmail.com>
2026-03-28 21:06:04 +0300

    added initial index.html for VMP frontend

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 
 home_bp = Blueprint("home", __name__)
 
+
+@home_bp.route("/")
+def index():
+    return render_template("index.html")
 
 @home_bp.route("/ping")
 def ping():
